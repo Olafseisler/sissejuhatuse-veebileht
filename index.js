513cd8ace78258b70d1ad492ca806bbcfd57c072
Olaf Seisler <olaf.seisler@gmail.com>
2021-11-26 17:56:21 +0200

    Muutsin laulude lehe javascriptist genereeritavaks

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+let laulud = [
+    { name: "sõitu minek", artist: "benakanister", album: "Kytus" },
+    { name: "toba", artist: "NKN", album: "Koosolek 4" },
+    { name: "Calabassas", artist: "SKP", album: "Supiköögi Sulased" },
+    { name: "Maja (Mitte Lõks)", artist: "benakanister", album: "Kytus" },
+    { name: "GYMN", artist: "Lil Ants", album: "" }
+];
+
 function plusSlides(n) {
     showSlides(slideIndex += n)
 }
@@ -26,6 +34,7 @@ function showSlides(n)  {
 
 let textSlideIndex = 1;
 showTextSlides(textSlideIndex);
+generateTable();
 
 function plusTextSlides(n) {
     showTextSlides(textSlideIndex += n)
@@ -45,3 +54,23 @@ function showTextSlides(n)  {
     }
     textSlides[textSlideIndex-1].style.display = "block";
 }
+
+function generateTable(){
+    table = document.getElementById("table");
+
+    for (let element of laulud) {
+        let row = document.createElement("tr")
+
+        let row_data_1 = document.createElement('td');
+        row_data_1.innerHTML = element.name;
+        let row_data_2 = document.createElement('td');
+        row_data_2.innerHTML = element.artist;
+        let row_data_3 = document.createElement('td');
+        row_data_3.innerHTML = element.album;
+
+        row.appendChild(row_data_1);
+        row.appendChild(row_data_2);
+        row.appendChild(row_data_3);
+        table.appendChild(row);
+    }
+}   
